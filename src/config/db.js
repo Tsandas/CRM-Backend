99a17cccd99c72d291b3819e46494a4d7f548d04
Tsandas <georgetsandekidis@gmail.com>
2025-11-23 10:36:40 +0200

    test koyeb deploy

--- src/config/db.js
+++ src/config/db.js
@@ -3,16 +3,27 @@ import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 const { Pool } = pg;
+
+let sslConfig;
+if (process.env.NODE_ENV === "production") {
+  sslConfig = {
+    rejectUnauthorized: true,
+    ca: process.env.CA,
+  };
+} else {
+  sslConfig = {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(process.env.CA).toString(),
+  };
+}
+
 const pool = new Pool({
   user: process.env.USER,
   password: process.env.PASSWORD,
   host: process.env.HOST,
   port: parseInt(process.env.PORT, 10),
   database: process.env.DATABASE,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync(process.env.CA).toString(),
-  },
+  ssl: sslConfig,
 });
 
 pool
